0797e4dd17470acf20064c61840b1e142af79bd9
Milad Nekofar <milad@nekofar.com>
2023-06-17 19:05:13 +0330

    feat: add Hono CLI Project generator
    This commit adds new files related to the Hono CLI Project generator plugin. Specifically, it adds HonoBundle.kt, HonoCliProjectGenerator.kt, HonoCliProjectModuleBuilder.kt, HonoProjectTemplateFactory.kt, and updates plugin.xml. The new plugin enables users to create a new project using Hono in IntelliJ IDEA.

---
+++ src/main/kotlin/com/nekofar/milad/intellij/hono/cli/HonoCliProjectGenerator.kt
@@ -0,0 +1,38 @@
+package com.nekofar.milad.intellij.hono.cli
+
+import com.intellij.execution.filters.Filter
+import com.intellij.lang.javascript.boilerplate.NpmPackageProjectGenerator
+import com.intellij.lang.javascript.boilerplate.NpxPackageDescriptor
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ContentEntry
+import com.intellij.openapi.vfs.VirtualFile
+import com.nekofar.milad.intellij.hono.HonoBundle
+import com.nekofar.milad.intellij.hono.HonoIcons
+import javax.swing.Icon
+
+class HonoCliProjectGenerator : NpmPackageProjectGenerator() {
+    private val packageName = "create-hono"
+    private val npxCommand = "create-hono"
+
+    override fun getName(): String = HonoBundle.message("hono.project.generator.name")
+
+    override fun getDescription(): String = HonoBundle.message("hono.project.generator.description")
+
+    override fun filters(project: Project, baseDir: VirtualFile): Array<Filter> = emptyArray()
+
+    override fun customizeModule(virtualFile: VirtualFile, contentEntry: ContentEntry?) {}
+
+    override fun packageName(): String = packageName
+
+    override fun presentablePackageName(): String =
+        HonoBundle.message("hono.project.generator.presentable.package.name")
+
+    override fun getNpxCommands() = listOf(NpxPackageDescriptor.NpxCommand(packageName, npxCommand))
+
+    override fun generateInTemp(): Boolean = false
+
+    override fun generatorArgs(project: Project?, dir: VirtualFile?, settings: Settings?): Array<String> =
+        arrayOf(".")
+
+    override fun getIcon(): Icon = HonoIcons.ProjectGenerator
+}
