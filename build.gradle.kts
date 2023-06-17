6ed826fbe61ade3af5df35fdc7cee381c9e59370
Milad Nekofar <milad@nekofar.com>
2023-06-17 16:24:29 +0330

    chore: add configuration files and scripts needed to build and run project
    This commit adds missing configuration files and scripts required for building and running the application, such as `.gitignore`, `build.gradle.kts`, `gradlew`, `gradle/wrapper/gradle-wrapper.jar`, `gradle/wrapper/gradle-wrapper.properties`, and `.run/Run IDE with Plugin.run.xml`. The configuration files were updated according to the project requirements, e.g., the Java version was set to 11, and some files do not need to be ignored anymore. All the files are necessary for the application to function correctly.

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.8.21"
+    id("org.jetbrains.intellij") version "1.13.3"
+}
+
+group = "com.nekofar.milad"
+version = "1.0.0-alpha.0"
+
+repositories {
+    mavenCentral()
+}
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    version.set("2021.3")
+    type.set("IU") // Target IDE Platform
+
+    plugins.set(listOf('JavaScript'))
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("213")
+        untilBuild.set("232.*")
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
