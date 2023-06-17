34084df3270763ba96f9fe945392270f1e86b294
Milad Nekofar <milad@nekofar.com>
2023-06-17 19:26:06 +0330

    build: setup description and changelog in `patchPluginXml`
    Configure Gradle Changelog Plugin and update plugin's manifest with a new version and plugin description along with getting the latest available change notes.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,12 @@
+import org.jetbrains.changelog.Changelog
+import org.jetbrains.changelog.markdownToHTML
+
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.8.21"
     id("org.jetbrains.intellij") version "1.13.3"
+    // Gradle Changelog Plugin
+    id("org.jetbrains.changelog") version "2.0.0"
 }
 
 group = "com.nekofar.milad"
@@ -20,6 +25,13 @@ intellij {
     plugins.set(listOf("JavaScript"))
 }
 
+// Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
+changelog {
+    version.set("1.0.0-alpha.0")
+    groups.set(emptyList())
+    repositoryUrl.set("https://github.com/KartanHQ/intellij-hono")
+}
+
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
@@ -31,8 +43,34 @@ tasks {
     }
 
     patchPluginXml {
+        version.set("1.0.0-alpha.0")
         sinceBuild.set("213")
         untilBuild.set("232.*")
+
+        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
+        pluginDescription.set(
+            file("README.md").readText().lines().run {
+                val start = "<!-- Plugin description -->"
+                val end = "<!-- Plugin description end -->"
+
+                if (!containsAll(listOf(start, end))) {
+                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                }
+                subList(indexOf(start) + 1, indexOf(end))
+            }.joinToString("\n").let { markdownToHTML(it) }
+        )
+
+        // Get the latest available change notes from the changelog file
+        changeNotes.set(provider {
+            with(changelog) {
+                val changelogItem = getOrNull("1.0.0-alpha.0")
+                    ?: runCatching { getLatest() }.getOrElse { getUnreleased() }
+                renderItem(
+                    changelogItem.withHeader(false),
+                    Changelog.OutputType.HTML,
+                )
+            }
+        })
     }
 
     signPlugin {
