62bfdc1c565a7113a14e287f1050162d139c393a
Milad Nekofar <milad@nekofar.com>
2023-07-10 09:54:12 +0330

    refactor: update project generator for better message handling
    Refactored the `HonoCliProjectGenerator` class to improve message handling by modifying the import statement for `HonoBundle` to import the `message` function. Updated the overridden methods `getName()`, `getDescription()`, `filters()`, `packageName()`, `presentablePackageName()`, and `getIcon()` to use the imported `message` function for retrieving the appropriate messages and values. Some methods were also adjusted to return specific types instead of using explicit type declarations. The changes aim to enhance the readability and maintainability of the code.

--- src/main/kotlin/com/nekofar/milad/intellij/hono/cli/HonoCliProjectGenerator.kt
+++ src/main/kotlin/com/nekofar/milad/intellij/hono/cli/HonoCliProjectGenerator.kt
@@ -6,7 +6,7 @@ import com.intellij.lang.javascript.boilerplate.NpxPackageDescriptor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ContentEntry
 import com.intellij.openapi.vfs.VirtualFile
-import com.nekofar.milad.intellij.hono.HonoBundle
+import com.nekofar.milad.intellij.hono.HonoBundle.message
 import com.nekofar.milad.intellij.hono.HonoIcons
 import javax.swing.Icon
 
@@ -14,24 +14,23 @@ class HonoCliProjectGenerator : NpmPackageProjectGenerator() {
     private val packageName = "create-hono"
     private val npxCommand = "create-hono"
 
-    override fun getName(): String = HonoBundle.message("hono.project.generator.name")
+    override fun getName() = message("hono.project.generator.name")
 
-    override fun getDescription(): String = HonoBundle.message("hono.project.generator.description")
+    override fun getDescription() = message("hono.project.generator.description")
 
-    override fun filters(project: Project, baseDir: VirtualFile): Array<Filter> = emptyArray()
+    override fun filters(project: Project, baseDir: VirtualFile) = emptyArray<Filter>()
 
     override fun customizeModule(virtualFile: VirtualFile, contentEntry: ContentEntry?) {}
 
-    override fun packageName(): String = packageName
+    override fun packageName() = packageName
 
-    override fun presentablePackageName(): String =
-        HonoBundle.message("hono.project.generator.presentable.package.name")
+    override fun presentablePackageName() = message("hono.project.generator.presentable.package.name")
 
     override fun getNpxCommands() = listOf(NpxPackageDescriptor.NpxCommand(packageName, npxCommand))
 
-    override fun generateInTemp(): Boolean = true
+    override fun generateInTemp() = true
 
     override fun generatorArgs(project: Project, baseDir: VirtualFile) = arrayOf(project.name)
 
-    override fun getIcon(): Icon = HonoIcons.ProjectGenerator
+    override fun getIcon() = HonoIcons.ProjectGenerator
 }
