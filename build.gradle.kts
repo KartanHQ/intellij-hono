5936095b2e7b4fc211cf5c7c82c14f49937cf806
Milad Nekofar <milad@nekofar.com>
2024-10-06 13:55:30 +0400

    chore(build): add IntelliJ platform repositories extension
    Enable default IntelliJ repositories to simplify dependency management.

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,11 @@ kotlin {
 // Configure project's dependencies
 repositories {
     mavenCentral()
+
+    // IntelliJ Platform Gradle Plugin Repositories Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-repositories-extension.html
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
